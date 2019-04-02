a762c3cf222313b5f82b2113178f79700523a1c6
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2019-04-03 07:32:17 +0800

    First commit

---
+++ lib/chakin/ruby.rb
@@ -0,0 +1,8 @@
+require "chakin/ruby/version"
+
+module Chakin
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/chakin/ruby/version.rb
@@ -0,0 +1,5 @@
+module Chakin
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
